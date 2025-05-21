ecbe9a9d11a17aa2a1fd82a0e78d1ac756b8a339
wazed-biplob <www.biplob@gmail.com>
2025-05-21 09:07:11 +0600

    deploy yaml

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
